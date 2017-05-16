911e2b0113f5b1200747ed7a6f3e2b8347a32017
kurt.su <kurt.su@irdeto.com>
2017-05-16 19:06:32 +0800

    use eclipse project classes.

--- tools/poisoner/samples/run_dex_poisoner_test.sh
+++ tools/poisoner/samples/run_dex_poisoner_test.sh
@@ -5,7 +5,8 @@ export PATH=/home/kurt/Work/tools/jdk/jdk1.6.0_45:$PATH
 
 #cp -r ../src/java/classesgenerated/iac.jar iac.jar
 
-class_path="../../poisoner/libs/commons-codec-1.6.jar:../../poisoner/libs/commons-io-2.1.jar:iac.jar"
+CONVERTER=/home/kurt/company/workspace.eclipse/J2NConverter
+class_path="../../poisoner/libs/commons-codec-1.6.jar:../../poisoner/libs/commons-io-2.1.jar:$CONVERTER/bin/"
 #class_path="../../poisoner/libs/commons-codec-1.6.jar:../../poisoner/libs/commons-io-2.1.jar:../../poisoner/libs/DexConvertor.jar"
 
 cmd="/home/kurt/Work/tools/jdk/jdk1.6.0_45/bin/java  -DConfigureFilePath="./hello_dexconfig.xml"   -classpath "$class_path" com.irdeto.secureaccess.android.dexreader.IrdetoDexConvertor classes.dex  out.jar"
