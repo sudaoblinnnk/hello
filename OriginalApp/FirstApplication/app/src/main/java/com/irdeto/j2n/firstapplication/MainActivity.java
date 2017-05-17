838b345ff2dec908589ed86fbffcaf2e0e3832d5
kurt.su <kurt.su@irdeto.com>
2017-05-17 14:57:26 +0800

    test project add internal class KeyLogic

--- OriginalApp/FirstApplication/app/src/main/java/com/irdeto/j2n/firstapplication/MainActivity.java
+++ OriginalApp/FirstApplication/app/src/main/java/com/irdeto/j2n/firstapplication/MainActivity.java
@@ -11,9 +11,28 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         add();
+
+        KeyLogic.getInstance().getValue();
     }
 
     private int add() {
         return 1+2;
+    }
+
+    private static class KeyLogic {
+        int value;
+        private KeyLogic(int value) {
+            this.value = value;
+        }
+
+        private static KeyLogic instance = new KeyLogic(1);
+
+        public static KeyLogic getInstance() {
+            return instance;
+        }
+
+        public int getValue() {
+            return KeyLogic.class.getSimpleName().hashCode();
+        }
     }
 }
