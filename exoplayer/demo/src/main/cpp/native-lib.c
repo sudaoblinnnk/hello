19fbc2cf2cb52913af2f118d25812bc4f6178faf
kurt.su <kurt.su@irdeto.com>
2017-09-21 15:57:43 +0800

    support transcode.

--- exoplayer/demo/src/main/cpp/native-lib.c
+++ exoplayer/demo/src/main/cpp/native-lib.c
@@ -1,22 +1,14 @@
-#include <jni.h>
-#include <stdio.h>
-#include <android/log.h>
 #include "registerNative.h"
 
-char * AC_DEBUG = "kurt";
-
-#define LOGI(fmt, args...) __android_log_print(ANDROID_LOG_INFO,  AC_DEBUG, fmt, ##args)
-#define LOGD(fmt, args...) __android_log_print(ANDROID_LOG_DEBUG, AC_DEBUG, fmt, ##args)
-#define LOGE(fmt, args...) __android_log_print(ANDROID_LOG_ERROR, AC_DEBUG, fmt, ##args)
-
-
-
+_xc_preserve_interface
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved)
 {
     JNIEnv *env; jint registerResult = JNI_FALSE;
     if ((*vm)->GetEnv(vm, (void **) &env, JNI_VERSION_1_4) != JNI_OK) { return -1; }
 
-    registerNativeSymbols420217909_442(env);
+    if (registerNativeSymbols420217909_442(env) == JNI_FALSE) {
+        return -1;
+    }
 
     return JNI_VERSION_1_4;
 }
--- exoplayer/demo/src/main/cpp/registerNative.h
+++ exoplayer/demo/src/main/cpp/registerNative.h
@@ -5,7 +5,25 @@
 #ifndef EXOPLAYER_REGISTERNATIVE_H
 #define EXOPLAYER_REGISTERNATIVE_H
 
+#ifdef  XC_ISO98
+#include <xc/xc_extensions.h>
 #include <jni.h>
+#include <stdio.h>
+#include <android/log.h>
+#else
+#include <jni.h>
+#include <android/log.h>
+#define _xc_preserve_interface
+#endif //XC_ISO98
+
+
+static char * AC_DEBUG = "kurt";
+
+#define LOGI(fmt, args...) __android_log_print(ANDROID_LOG_INFO,  AC_DEBUG, fmt, ##args)
+#define LOGD(fmt, args...) __android_log_print(ANDROID_LOG_DEBUG, AC_DEBUG, fmt, ##args)
+#define LOGE(fmt, args...) __android_log_print(ANDROID_LOG_ERROR, AC_DEBUG, fmt, ##args)
+
+
 
 int registerNativeSymbols420217909_442(JNIEnv * env);
 
